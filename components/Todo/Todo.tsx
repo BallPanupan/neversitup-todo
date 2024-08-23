369963f03cc413ba1a7ef039709cdecb7876ad96
Panupan <Ball085573@hotmail.com>
2024-08-23 20:13:42 +0700

    fix(component): todo component css

--- components/Todo/Todo.tsx
+++ components/Todo/Todo.tsx
@@ -16,11 +16,11 @@ export default function Todo(props: any) {
 		<div className={styles.todo}>
 			
 			<div className='d-flex justify-content-between'>
-				<div><h4> { title || 'title' } </h4></div>
+				<div className='cursor-pointer'><h4> { title || 'title' } </h4></div>
 				<div className='cursor-pointer' onClick={()=> handlerRemoveTodo({id, title, content, createDate})}>close</div>
 			</div>
 
-			<div>
+			<div className='cursor-pointer'>
 				{ content || 'content' }
 			</div>
 			<div className={`${styles.date} d-flex flex-direction-reverse`}>{ createDate || 'createDate' }</div>
